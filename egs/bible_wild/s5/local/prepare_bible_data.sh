731a9f6139964fb43e76db8e624a6e70efb556f5
Matthew Wiesner <wiesner@jhu.edu>
2019-12-18 15:50:24 -0500

    Updates monolingual prepapre_bible_data.sh script to work with mp3

--- egs/bible_wild/s5/local/prepare_bible_data.sh
+++ egs/bible_wild/s5/local/prepare_bible_data.sh
@@ -12,40 +12,47 @@ affix=
 echo "AFFIX: ${affix}"
 echo "data/dict_${langid}${affix}"
 
-data=${!langid}
+langdata=${!langid}
 wordlists=data/local/${langid}/text
 
 echo ${charset}
 
-langdata=data/${langid}
-mkdir -p ${langdata}
+datadir=data/${langid}
+mkdir -p ${datadir}
+
+echo "Lang data: ${langdata}"
 
 #Make wav.scp
-files=( `find -L ${data}/aligned/wav -name *.wav` )
+files=( `find -L ${langdata}/wav -name "*.mp3"` )
 for f in ${files[@]}; do
   fname=`basename $f`
-  fname=${fname%%.wav}   
-  echo "${fname} sox ${f} -t wav -r 16000 -|"
-done | sort > ${langdata}/wav.scp
+  fname=${fname%%.mp3}
+  echo "${fname} ffmpeg -v 8 -i ${f} -f wav -acodec pcm_s16le - | sox -t wav - -r 16000 -c 1 -t wav -|"
+  # sox should work, but it sometimes doesn't have mp3 support. 
+  #echo "${fname} sox ${f} -t wav -r 16000 -|"
+done | sort > ${datadir}/wav.scp
+
+# Get segments file
+awk '{print $2,$1,$3,$4}' ${langdata}/asr_files/segments > ${datadir}/segments
 
 # Make utt2spk and spk2utt
-awk '{print $1" "$1}' ${langdata}/wav.scp > ${langdata}/utt2spk
-./utils/spk2utt_to_utt2spk.pl ${langdata}/utt2spk > ${langdata}/spk2utt
+awk '{print $1" "$1}' ${datadir}/segments > ${datadir}/utt2spk
+./utils/spk2utt_to_utt2spk.pl ${datadir}/utt2spk > ${datadir}/spk2utt
 
 # Make text 
 mkdir -p ${wordlists}
-echo "Searching in ${data} for text ..."
-find ${data}/download/txt -name "*.txt" > ${wordlists}/${langid}_files.scp
+echo "Searching in ${langdata} for text ..."
+find ${langdata}/download/txt -name "*.txt" > ${wordlists}/${langid}_files.scp
 ./local/unicode_filter_punct.py data/local/${langid}/text/${langid}_files.scp data/local/${langid}/text/text
 
-# Get tokens and filter by script
+# Get tokens and filter out numbers so that they will become unk
 cut -d' ' -f2- data/local/${langid}/text/text | ngram-count -order 1 -text - -write data/local/${langid}/text/tokens
 ./local/filter.py data/local/${langid}/text/tokens data/local/${langid}/text/vocab
 awk '{print $1}' data/local/${langid}/text/vocab > data/local/${langid}/text/words
 
 LC_ALL= python local/clean_transcript.py \
   ${!langid}/asr_files/transcription_nopunc.txt data/local/${langid}/text/words \
-  > ${langdata}/text
+  > ${datadir}/text
 
 localdict=data/local/${langid}/dict_${langid}${affix}
 mkdir -p $localdict
@@ -55,29 +62,29 @@ paste -d' ' data/local/${langid}/text/words \
 
 # Make splits
 echo "Using user specified subsets ..."
-cp -r ${langdata} ${langdata}_train
-cp -r ${langdata} ${langdata}_dev
-cp -r ${langdata} ${langdata}_eval
+cp -r ${datadir} ${datadir}_train
+cp -r ${datadir} ${datadir}_dev
+cp -r ${datadir} ${datadir}_eval
 
-grep -f dev.list ${langdata}/text | cut -f1  > data/local/${langid}/dev.keys
-grep -f eval.list ${langdata}/text | cut -f1 > data/local/${langid}/eval.keys
+grep -f dev.list ${datadir}/text | cut -f1  > data/local/${langid}/dev.keys
+grep -f eval.list ${datadir}/text | cut -f1 > data/local/${langid}/eval.keys
 
 dev_keys=data/local/${langid}/dev.keys
 eval_keys=data/local/${langid}/eval.keys
 
-./utils/filter_scp.pl --exclude -f 1 $dev_keys ${langdata}/text > ${langdata}_train/text.nodev 
-./utils/filter_scp.pl --exclude -f 1 $eval_keys ${langdata}_train/text.nodev > ${langdata}_train/text
-./utils/filter_scp.pl -f 1 $dev_keys ${langdata}/text > ${langdata}_dev/text 
-./utils/filter_scp.pl -f 1 $eval_keys ${langdata}/text > ${langdata}_eval/text 
+./utils/filter_scp.pl --exclude -f 1 $dev_keys ${datadir}/text > ${datadir}_train/text.nodev 
+./utils/filter_scp.pl --exclude -f 1 $eval_keys ${datadir}_train/text.nodev > ${datadir}_train/text
+./utils/filter_scp.pl -f 1 $dev_keys ${datadir}/text > ${datadir}_dev/text 
+./utils/filter_scp.pl -f 1 $eval_keys ${datadir}/text > ${datadir}_eval/text 
 
-./utils/fix_data_dir.sh ${langdata}_train
-./utils/fix_data_dir.sh ${langdata}_dev
-./utils/fix_data_dir.sh ${langdata}_eval
+./utils/fix_data_dir.sh ${datadir}_train
+./utils/fix_data_dir.sh ${datadir}_dev
+./utils/fix_data_dir.sh ${datadir}_eval
 
 # Get training words
 dict=data/dict_${langid}${affix}
 mkdir -p ${dict}
-cut -f2- ${langdata}_train/text | tr " " "\n" | sort -u |\
+cut -f2- ${datadir}_train/text | tr " " "\n" | sort -u |\
   awk '(NR==FNR){a[$1]=1; next} ($1 in a){print $0}' - ${localdict}/lexicon.txt \
   | cat <(echo -e "<unk> SIL") - > ${dict}/lexicon.txt 
 
